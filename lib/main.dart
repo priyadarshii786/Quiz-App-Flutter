57d9d745507d44aa9905d5076bcb555239c580fc
priyadarshii786 <tathagatp918@gmail.com>
2024-01-12 02:24:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
